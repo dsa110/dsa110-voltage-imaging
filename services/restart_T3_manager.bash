115cd812a6da3e211e6b2923837e13ea02f43a1d
ubuntu <ubuntu@pm.me>
2021-08-25 06:00:17 +0000

    labeling, bug fixes

---
+++ services/restart_T3_manager.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+
+systemctl --user stop send_cands.service
+systemctl --user stop dask_scheduler.service
+systemctl --user stop dask_worker1.service
+systemctl --user stop dask_worker2.service
+systemctl --user stop dask_worker3.service
+systemctl --user stop dask_worker4.service
+
+sleep 2
+
+systemctl --user start dask_scheduler.service
+systemctl --user start dask_worker1.service
+systemctl --user start dask_worker2.service
+systemctl --user start dask_worker3.service
+systemctl --user start dask_worker4.service
+systemctl --user start send_cands.service
+
+
