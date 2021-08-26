ea342410495ce3dcbd6c4b50282f5768bfcea207
ubuntu <ubuntu@pm.me>
2021-08-26 01:10:46 +0000

    voltage version

--- dsaT3/T3_manager.py
+++ dsaT3/T3_manager.py
@@ -5,7 +5,7 @@ ds = dsa_store.DsaStore()
 import time, os
 import json
 
-TIMEOUT_FIL = 300
+TIMEOUT_FIL = 60
 TIMEOUT_CORR = 21600
 FILPATH = '/data/dsa110/T1/'
 OUTPUT_PATH = '/home/ubuntu/data/T3/'
@@ -20,7 +20,15 @@ def fill_empty_dict(od):
     for corr in ['corr03','corr04','corr05','corr06','corr07','corr08','corr10','corr11','corr12','corr14','corr15','corr16','corr18','corr19','corr21','corr22']:
         od[corr+'_data'] = None
         od[corr+'_header'] = None
-    
+
+
+# searches for local file
+def search_for_local_file(fl):
+
+    if os.path.exists(fl):
+        return fl
+    return None
+        
 
 # waits for local file to be written
 def wait_for_local_file(fl,timt):
@@ -54,6 +62,49 @@ def run(a):
     filfile = '/data/dsa110/T1/' + corrXX + '/' + datestring + '/fil_' + output_dict['trigname'] + '/' + output_dict['trigname'] +	'_' + str(ibeam) + '.fil'
     print(filfile)
     found_filfile = wait_for_local_file(filfile,TIMEOUT_FIL)
+
+    if found_filfile is None:
+
+        with open(OUTPUT_PATH + output_dict['trigname'] + '.json', 'w') as f: #encoding='utf-8'
+            json.dump(output_dict, f, ensure_ascii=False, indent=4)
+        
+        return output_dict
+    
+    # launch candplotter
+    try:
+        output_dict['candplot'] = filf.filplot_entry(datestring,a)
+    except:
+        print('Could not make filplot '+output_dict['trigname'])
+        with open(OUTPUT_PATH + output_dict['trigname'] + '.json', 'w') as f: #encoding='utf-8'
+            json.dump(output_dict, f, ensure_ascii=False, indent=4)
+
+        return output_dict
+
+    # wait for voltage files to be written
+    
+
+    # write output_dict to disk
+    with open(OUTPUT_PATH + output_dict['trigname'] + '.json', 'w') as f: #encoding='utf-8'                  
+        json.dump(output_dict, f, ensure_ascii=False, indent=4)
+
+    return output_dict
+
+# a is T3 trigger dict
+def run_nowait(a):
+
+    # set up output dict and datestring
+    datestring = ds.get_dict('/cnf/datestring')
+    output_dict = a[list(a.keys())[0]]
+    output_dict['trigname'] = list(a.keys())[0]
+    output_dict['datestring'] = datestring
+    fill_empty_dict(output_dict)
+
+    # wait for specific filterbank file to be written
+    ibeam = output_dict['ibeam'] + 1
+    corrXX = FIL_CORRS[int( (ibeam-1) / 64)]
+    filfile = '/data/dsa110/T1/' + corrXX + '/' + datestring + '/fil_' + output_dict['trigname'] + '/' + output_dict['trigname'] +	'_' + str(ibeam) + '.fil'
+    print(filfile)
+    found_filfile = search_for_local_file(filfile)
 
     if found_filfile is None:
 
