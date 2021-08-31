9c2470b53d51de8b94ed5771d7dc0cabed7c1687
ubuntu <ubuntu@pm.me>
2021-08-31 15:40:42 +0000

    changes for dsa64

--- dsaT3/voltages_to_ms.py
+++ dsaT3/voltages_to_ms.py
@@ -14,53 +14,55 @@ import time
 import yaml
 from pkg_resources import resource_filename
 from astropy.time import Time
+import astropy.units as u
 from dsaT3.utils import get_declination_mjd, rsync_file
 from dsaT3.T3imaging import generate_T3_uvh5
 from dsacalib.ms_io import uvh5_to_ms
 
-CORRDIR = '/home/ubuntu/data/'
-NPROC = 3
+CORRDIR = '/media/ubuntu/ssd/data/B0329/'
+NPROC = 8
 PARAMFILE = resource_filename('dsaT3', 'data/T3_parameters.yaml')
 with open(PARAMFILE) as YAMLF:
     T3PARAMS = yaml.load(YAMLF, Loader=yaml.FullLoader)['T3corr']
 
-def rsync_handler(
-        rsync_queue,
-        corr_queue,
-        nvoltagefiles,
-        nvoltagefiles_lock,
-        rsync_done
-):
-    while not rsync_done.is_set():
-        if nvoltagefiles.value > 2:
-            time.sleep(10)
-            continue
-        try:
-            item = rsync_queue.get()
-        except queue.Empty:
-            time.sleep(10)
-        else:
-            if item == 'END':
-                rsync_done.set()
-                continue
-            with nvoltagefiles_lock:
-                nvoltagefiles.value += 1
-            srcfile, vfile = item
-            if not os.path.exists('{0}.corr'.format(vfile)):
-                rsync_file(
-                    srcfile,
-                    vfile
-                )
-            corr_queue.put(vfile)
+#def rsync_handler(
+#        rsync_queue,
+#        corr_queue,
+#        nvoltagefiles,
+#        nvoltagefiles_lock,
+#        # rsync_done
+#):
+#    rsync_done = False
+#    while not rsync_done:
+#        if nvoltagefiles.value >= NPROC:
+#            time.sleep(10)
+#            continue
+#        try:
+#            item = rsync_queue.get()
+#        except queue.Empty:
+#            time.sleep(10)
+#        else:
+#            if item == 'END':
+#                rsync_done = True
+#                continue
+#            with nvoltagefiles_lock:
+#                nvoltagefiles.value += 1
+#            srcfile, vfile = item
+#            #if not os.path.exists('{0}.corr'.format(vfile)):
+#            #    rsync_file(
+#            #        srcfile,
+#            #        vfile
+#            #    )
+#            corr_queue.put(vfile)
 
 def corr_handler(
         deltat_ms,
         deltaf_MHz,
         corr_queue,
         uvh5_queue,
-        corr_done,
-        nvoltagefiles,
-        nvoltagefiles_lock,
+        # corr_done,
+        #nvoltagefiles,
+        #nvoltagefiles_lock,
         ncorrfiles,
         ncorrfiles_lock
 ):
@@ -73,8 +75,9 @@ def corr_handler(
     deltaf_MHz : float
         The desired integration frequency in the correlated data, in MHz.
     """
-    while not corr_done.is_set():
-        if ncorrfiles.value > 2:
+    corr_done = False
+    while not corr_done:
+        if ncorrfiles.value > NPROC-1:
             time.sleep(10)
             continue
         try:
@@ -83,7 +86,7 @@ def corr_handler(
             time.sleep(10)
         else:
             if vfile == 'END':
-                corr_done.set()
+                corr_done = True
                 continue
             with ncorrfiles_lock:
                 ncorrfiles.value += 1
@@ -104,10 +107,10 @@ def corr_handler(
                 )
                 proc_stdout = str(process.communicate()[0].strip())
                 print(proc_stdout)
-            if os.path.exists(vfile):
-                os.remove(vfile)
-            with nvoltagefiles_lock:
-                nvoltagefiles.value -= 1
+            #if os.path.exists(vfile):
+            #    os.remove(vfile)
+            #with nvoltagefiles_lock:
+            #    nvoltagefiles.value -= 1
             corr_files = dict({})
             corr = re.findall('corr\d\d', vfile)[0]
             corr_files[corr] = '{0}.corr'.format(vfile)
@@ -122,13 +125,14 @@ def uvh5_handler(
         start_offset,
         end_offset,
         uvh5_queue,
-        uvh5_done,
+        #uvh5_done,
         ncorrfiles,
         ncorrfiles_lock
 ):
     proc = multiprocessing.current_process()
-    while not uvh5_done.is_set():
-        print(uvh5_done.is_set())
+    uvh5_done = False
+    while not uvh5_done:
+        #print(uvh5_done.is_set())
         try:
             corr_files = uvh5_queue.get()
         except queue.Empty:
@@ -136,8 +140,9 @@ def uvh5_handler(
         else:
             if corr_files == 'END':
                 print('proc {0} is setting uvh5_done'.format(proc.pid))
-                uvh5_done.set()
-                return
+                uvh5_done = True
+                #uvh5_done.set()
+                continue
             uvh5name = generate_T3_uvh5(
                 '{0}/{1}'.format(CORRDIR, name),
                 declination,
@@ -149,8 +154,8 @@ def uvh5_handler(
                 end_offset=end_offset
             )
             print(uvh5name)
-            for value in corr_files.values():
-                os.remove(value)
+            #for value in corr_files.values():
+            #    os.remove(value)
             with ncorrfiles_lock:
                 ncorrfiles.value -= 1
     print('{0} exiting'.format(proc.pid))
@@ -179,50 +184,60 @@ def __main__(name, filelist, ntint, nfint, start_offset, end_offset):
         set. If not provide,d the entire time is converted to a measurement
         set.
     """
+    if start_offset < 0:
+        start_offset = None
+    if end_offset < 0:
+        end_offset = None
     # Get metadata
-    filename = '{0}.json'.format(filelist[0])
-    outfile = rsync_file(filename, CORRDIR)
+    filename = '{0}.json'.format(filelist[0][:-4])
+    #outfile = rsync_file(filename, CORRDIR)
     with open(outfile) as jsonf:
         metadata = json.load(jsonf)#[name]
         key = list(metadata.keys())[0]
         metadata = metadata[key]
     tstart = Time(metadata['mjds'], format='mjd')
-    declination = get_declination_mjd(tstart)
+    #try:
+    #    declination = get_declination_mjd(tstart)
+    #except ConnectionError:
+    declination = 54.58209895*u.deg
     deltat_ms = ntint*T3PARAMS['deltat_s']*1e3
     deltaf_MHz = T3PARAMS['deltaf_MHz']
 
     manager = Manager()
-    nvoltagefiles = manager.Value('i', 0)
-    nvoltagefiles_lock = manager.Lock()
+    #nvoltagefiles = manager.Value('i', 0)
+    #nvoltagefiles_lock = manager.Lock()
     ncorrfiles = manager.Value('i', 0)
     ncorrfiles_lock = manager.Lock()
-    rsync_queue = manager.Queue()
+    #rsync_queue = manager.Queue()
     corr_queue = manager.Queue()
     uvh5_queue = manager.Queue()
-    rsync_done = manager.Event()
-    corr_done = manager.Event()
-    uvh5_done = manager.Event()
+    #rsync_done = manager.Event()
+    #corr_done = manager.Event()
+    #uvh5_done = manager.Event()
     # Copy files
     # Do 3 at a time
     for filename in filelist:
-        corr = re.findall('corr\d\d', filename)[0]
-        fname = filename.split('/')[-1]
-        outfile = '{0}/{1}_{2}'.format(CORRDIR, corr, fname)
-        print(filename, outfile)
-        rsync_queue.put([filename, outfile])
-    rsync_queue.put('END')
+        #corr = re.findall('corr\d\d', filename)[0]
+        #fname = filename.split('/')[-1]
+        #outfile = '{0}/{1}_{2}'.format(CORRDIR, corr, fname)
+        #print(filename, outfile)
+        #rsync_queue.put([filename, outfile])
+        corr_queue.put(filename)
+    for i in range(NPROC):
+        corr_queue.put('END')
+    #rsync_queue.put('END')
     processes = []
-    processes += [Process(
-        target=rsync_handler,
-        args=(
-            rsync_queue,
-            corr_queue,
-            nvoltagefiles,
-            nvoltagefiles_lock,
-            rsync_done
-            ),
-        daemon=True
-    )]
+    #processes += [Process(
+    #    target=rsync_handler,
+    #    args=(
+    #        rsync_queue,
+    #        corr_queue,
+    #        nvoltagefiles,
+    #        nvoltagefiles_lock,
+    #        #rsync_done
+    #        ),
+    #    daemon=True
+    #)]
     for i in range(NPROC):
         processes += [Process(
             target=corr_handler,
@@ -231,9 +246,9 @@ def __main__(name, filelist, ntint, nfint, start_offset, end_offset):
                 deltaf_MHz,
                 corr_queue,
                 uvh5_queue,
-                corr_done,
-                nvoltagefiles,
-                nvoltagefiles_lock,
+                #corr_done,
+                #nvoltagefiles,
+                #nvoltagefiles_lock,
                 ncorrfiles,
                 ncorrfiles_lock
             ),
@@ -251,7 +266,7 @@ def __main__(name, filelist, ntint, nfint, start_offset, end_offset):
                 start_offset,
                 end_offset,
                 uvh5_queue,
-                uvh5_done,
+                #uvh5_done,
                 ncorrfiles,
                 ncorrfiles_lock
             ),
@@ -259,29 +274,23 @@ def __main__(name, filelist, ntint, nfint, start_offset, end_offset):
         )]
     for proc in processes:
         proc.start()
-    processes[0].join()
-    corr_queue.put('END')
-    print('All rsync processes done')
-    for proc in processes[1:1+NPROC]:
+    for proc in processes[:NPROC]:
         proc.join()
     print('All corr processes done')
     # We get here
-    uvh5_queue.put('END')
-    for proc in processes[1+NPROC:]:
+    for i in range(NPROC):
+        uvh5_queue.put('END')
+    for proc in processes[NPROC:]:
         proc.join()
         print('A uvh5 process joined.')
     print('All uvh5 processes done')
-    # We arent getting here
-    # Is it possible the events are not being shared properly?
-    #hdf5files = sorted(glob.glob('{0}/{1}_corr??.hdf5'.format(
-    #    CORRDIR, name
-    #)))
-    # uvh5_to_ms(
-    #     hdf5files,
-    #     '{0}/{1}'.format(T3PARAMS['msdir'], name)
-    # )
-    #for file in hdf5files:
-    #    os.remove(file)
+    hdf5files = sorted(glob.glob('{0}/{1}_corr??.hdf5'.format(
+        CORRDIR, name
+    )))
+    uvh5_to_ms(
+        hdf5files,
+        '{0}/{1}'.format(T3PARAMS['msdir'], name)
+    )
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
