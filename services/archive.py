06219ad05ddb5ac76050cc3854df14872b7f4997
ubuntu <ubuntu@pm.me>
2021-10-05 16:43:36 +0000

    archive edit

--- services/archive.py
+++ services/archive.py
@@ -11,20 +11,21 @@ os.system('cp /home/ubuntu/data/T3/* '+T3root+datestring)
 
 fls = glob.glob(T3root+datestring+'/*.json')
 
-# let me know which ones will be archived
-for fl in fls:
-    f = open(fl)
-    de = json.load(f)
-    print('Key <save> not in {0}'.format(fl))
-    # Skip corr node json files without the save key if OoD archives twice
-    if de.get('save', False):
-        print('Will save voltages for ',de['trigname'])
+## let me know which ones will be archived
+#for fl in fls:
+#    f = open(fl)
+#    de = json.load(f)
+#    print('Key <save> not in {0}'.format(fl))
+#    # Skip corr node json files without the save key if OoD archives twice
+#    if de.get('save', False):
+#        print('Will save voltages for ',de['trigname'])
 
 for fl in fls:
 
     f = open(fl)
     de = json.load(f)
-    if de.get('save', False):        
+    #if de.get('save', False):
+    if de['label']=='astrophysical':
 
         for corr in ['corr03', 'corr04', 'corr05', 'corr06', 'corr07', 'corr08', 'corr10', 'corr11', 'corr12', 'corr14', 'corr15', 'corr16', 'corr18', 'corr19', 'corr21', 'corr22']:
 
