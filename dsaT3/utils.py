2ec84e78b7c15130163772b90933eb0b6669a9b9
ubuntu <ubuntu@pm.me>
2021-10-11 15:57:08 +0000

    archiving update

--- dsaT3/utils.py
+++ dsaT3/utils.py
@@ -2,43 +2,46 @@
 """
 import subprocess
 import json, os, glob
+import pipes
 
+def delete_remote(host, path):
+    """Delete remote file"""
+    status = subprocess.call(['ssh', host, 'rm -rf {}'.format(pipes.quote(path))])
+    return status
+    raise Exception('SSH failed')
 
-def archive(datestring, T3root='/media/ubuntu/data/dsa110/T3/'):
-    """ Archive data from corr nodes to local (h23).
-    Function form of original services/archive.py script.
-    """
+    
+def exists_remote(host, path):
+    """Test if a file exists at path on a host accessible with SSH."""
+    status = subprocess.call(
+        ['ssh', host, 'test -f {}'.format(pipes.quote(path))])
+    if status == 0:
+        return True
+    if status == 1:
+        return False
+    raise Exception('SSH failed')
 
-    # copy from T3 directory 
-    os.system('mkdir -p '+T3root+datestring)
-    os.system('rsync -av /home/ubuntu/data/T3/ '+T3root+datestring)
+def check_voltages(candname):
 
-    fls = glob.glob(T3root+datestring+'/*.json')
-    saved_trigname = []
+    filename = f'/home/ubuntu/data/T3/{candname}.json'
+    assert os.path.exists(filename), f'candidate json file {filename} not found'
+    dd = readfile(filename)
+    
+    corrs = ['corr03','corr04','corr05','corr06','corr07','corr08','corr10','corr11','corr12','corr14','corr15','corr16','corr18','corr19','corr21','corr22']
 
-    for fl in fls:
-        f = open(fl)
-        de = json.load(f)
-#        print('Key <save> not in {0}'.format(fl))
-        # Skip corr node json files without the save key if OoD archives twice
-        if de.get('save', False):
-            print('Will save voltages for ', de['trigname'])
-            saved_trigname.append(de['trigname'])
+    # edit corr03_data and corr03_header
+    for corr in corrs:
 
-            for corr in ['corr03', 'corr04', 'corr05', 'corr06', 'corr07', 'corr08', 'corr10', 'corr11', 'corr12', 'corr14', 'corr15', 'corr16', 'corr18', 'corr19', 'corr21', 'corr22']:
-                if de[corr+'_header'] is True:
-                    outfile_h = T3root + datestring + '/'+corr+'_'+de['trigname']+'_header.json'
-                    if not os.path.exists(outfile_h):
-                        print('copying header '+corr+' '+de['trigname'])
-                        os.system('scp '+corr+'.sas.pvt:./data/'+de['trigname']+'_header.json '+outfile_h)
-            
-                if de[corr+'_data'] is True:
-                    outfile_d = T3root + datestring + '/'+corr+'_'+de['trigname']+'_data.out'
-                    if not os.path.exists(outfile_d):
-                        print('copying data '+corr+' '+de['trigname'])
-                        os.system('scp '+corr+'.sas.pvt:./data/'+de['trigname']+'_data.out '+outfile_d)
+        data_file = '/home/ubuntu/data/'+candname+'_data.out'
+        header_file = '/home/ubuntu/data/'+candname+'_header.json'
+        if exists_remote(corr+'.sas.pvt',data_file):
+            dd[corr+'_data'] = True
+            print('Found data:',corr)
+        if exists_remote(corr+'.sas.pvt',header_file):
+            dd[corr+'_header'] = True
+            print('Found header:',corr)
 
-    return saved_trigname        
+    writefile(dd, filename)
 
 def rsync_file(infile, outfile):
     """Rsyncs a file from the correlator machines.
@@ -67,3 +70,44 @@ def rsync_file(infile, outfile):
     if process.returncode != 0:
         return None
     return outfile
+
+
+def archive(datestring, T3root='/media/ubuntu/data/dsa110/T3/'):
+    """ Archive data from corr nodes to local (h23).
+    Function form of original services/archive.py script.
+    """
+
+    # copy from T3 directory 
+    os.system('mkdir -p '+T3root+datestring)
+    os.system('rsync -av /home/ubuntu/data/T3/ '+T3root+datestring)
+
+    fls = glob.glob(T3root+datestring+'/*.json')
+    saved_trigname = []
+
+    for fl in fls:
+
+        f = open(fl)
+        de = json.load(f)
+
+        # if save is true, scp voltage files
+        if de['save'] is True:
+
+            saved_trigname.append(de['trigname'])
+
+            for corr in ['corr03', 'corr04', 'corr05', 'corr06', 'corr07', 'corr08', 'corr10', 'corr11', 'corr12', 'corr14', 'corr15', 'corr16', 'corr18', 'corr19', 'corr21', 'corr22']:
+                
+                outfile_h = T3root + datestring + '/'+corr+'_'+de['trigname']+'_header.json'
+                infile_h = corr+'.sas.pvt:/home/ubuntu/data/'+de['trigname']+'_header.json'
+                outfile_d = T3root + datestring + '/'+corr+'_'+de['trigname']+'_data.out'
+                infile_d = corr+'.sas.pvt:/home/ubuntu/data/'+de['trigname']+'_data.out'
+
+                print('Searching for remote file '+'/home/ubuntu/data/'+de['trigname']+'_data.out')
+                if exists_remote(corr+'.sas.pvt','/home/ubuntu/data/'+de['trigname']+'_data.out'):
+                    if not os.path.exists(outfile_h):
+                        rsync_file(infile_h,outfile_h)
+                    if not os.path.exists(outfile_d):
+                        print('Copying...')
+                        rsync_file(infile_d,outfile_d)                                    
+            
+    return saved_trigname        
+
