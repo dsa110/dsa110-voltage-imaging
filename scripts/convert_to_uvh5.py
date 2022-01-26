dd415b6b3cd220e2cb7232439b67d776a963b371
ubuntu <ubuntu@pm.me>
2022-01-26 21:31:36 +0000

    Convert each subband to its own ms

--- scripts/convert_to_uvh5.py
+++ scripts/convert_to_uvh5.py
@@ -1,15 +1,14 @@
 from dsacalib.ms_io import uvh5_to_ms
 import glob
 
-for name in ['hydraAaaa_full']:
-    msname = '/media/ubuntu/data/dsa110/imaging/{0}'.format(name)
+for name in ['220121aaat']:
     uvh5files = sorted(glob.glob(
-        '/media/ubuntu/data/dsa110/imaging/{0}_corr??.hdf5'.format(name)
+        '/media/ubuntu/ssd/data/{0}_corr??.hdf5'.format(name)
     ))
-    print(msname)
-    print(len(uvh5files))
-    print(uvh5files)
-    uvh5_to_ms(
-        uvh5files,
-        msname
-    )
+    for uvh5file in uvh5files:
+        msname = '/media/ubuntu/data/dsa110/imaging/{0}'.format(
+            uvh5file.replace('.hdf5', '').split('/')[-1])
+        uvh5_to_ms(
+            uvh5file,
+            msname
+        )
