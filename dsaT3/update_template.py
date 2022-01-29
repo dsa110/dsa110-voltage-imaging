ba54694e200cf2562b801069577e54388e1e5a9b
ubuntu <ubuntu@pm.me>
2022-01-29 00:50:04 +0000

    Add weights to measurement set

--- dsaT3/update_template.py
+++ dsaT3/update_template.py
@@ -32,6 +32,46 @@ def update_template(template_filepath: str, uvh5_filepaths: list):
     update_visibilities(template_filepath, uvh5_filepaths)
     update_metadata(template_filepath, uvh5_filepaths[0])
 
+def update_weights(sigma_spectrum: np.ndarray, template_filepath: str) -> None:
+    """Updates a template file with weights based on pulse profile and DM.
+    
+    `sigma_spectrum` should have dimensions (Nblt, Nchans, Npols).
+    """
+    weight_spectrum = np.power(sigma_spectrum, -2)
+    weight = weight_spectrum.sum(axis=1)
+    sigma = np.power(weight, -0.5)
+    
+    with table(template_filepath) as tb:
+        has_sigma_spectrum = 'SIGMA_SPECTRUM' in tb.colnames()
+    if not has_sigma_spectrum:
+        add_sigma_spectrum_column(template_filepath)
+
+    with table(template_filepath, readonly=False) as tb:
+        tb.putcol('SIGMA_SPECTRUM', sigma_spectrum)
+        tb.putcol('WEIGHT_SPECTRUM', weight_spectrum)
+        tb.putcol('WEIGHT', weight)
+        tb.putcol('SIGMA', sigma)
+        
+def add_sigma_spectrum_column(template_filepath: str) -> None:
+    """Add SIGMA_SPECTRUM as a column to the measurement set at `template_filepath`"""
+    sigma_spectrum_desc = get_sigma_spectrum_desc()
+    with table(template_filepath, readonly=False) as tb:
+        tb.addcols(sigma_spectrum_desc)
+
+def get_sigma_spectrum_desc() -> dict:
+    """Generate the column description for SIGMA_SPECTRUM"""
+    sigma_spectrum_desc = {'SIGMA_SPECTRUM':{
+        'valueType': 'float',
+        'dataManagerType': 'TiledShapeStMan',
+        'dataManagerGroup': 'TiledSigmaSpectrum',
+        'option': 0,
+        'maxlen': 0,
+        'comment': 'Estimated rms noise for each data point',
+        'ndim': 2,
+        '_c_order': True,
+        'keywords': {}}}
+    return sigma_spectrum_desc
+
 def update_visibilities(template_filepath: str, uvh5_filepaths: list, n_corr_nodes: int=N_CORR_NODES):
     """Updates a template file with real data.
 
@@ -123,13 +163,6 @@ def calculate_blen(uvh5file: UVData) -> np.ndarray:
         ant2 = uvh5file.ant_2_array[i]
         blen[i, ...] = uvh5file.antenna_positions[ant2, :] - uvh5file.antenna_positions[ant1, :]
     return blen
-
-def update_weights(simulated_dynspec: np.ndarray, template_filepath: str) -> None:
-    """Updates a template file with weights based on pulse profile and DM.
-    
-    Here, we need to set sigma_spectrum and sigma; weight_spectrum and weight.
-    """
-    pass
 
 class TemplateMSMD():
     """Access and update the metatdata of the template ms."""
