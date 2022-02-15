e345e58c8dcd344816e319c865cf8b5ffbdb08db
ubuntu <ubuntu@pm.me>
2022-02-15 17:17:54 +0000

    Bugfix: wrong variable name

--- dsaT3/uvh5_to_ms.py
+++ dsaT3/uvh5_to_ms.py
@@ -5,7 +5,7 @@ import re
 import numpy as np
 from dsacalib.uvh5_to_ms import load_uvh5_file, set_antenna_positions, phase_visibilities
 from dsacalib.uvh5_to_ms import fix_descending_missing_freqs, write_UV_to_ms
-from dsaT3.dedisperse import dedisperse
+from dsaT3.dedisperse import dedisperse_and_select_times
 from dsaT3.update_template import update_metadata, TemplateMSVis
 from casacore.tables import tablecopy
 from casatasks import virtualconcat
@@ -24,7 +24,7 @@ DAY_TO_S = DAY_TO_MS/1e3
 REF_FREQ_GHZ = 1.530
 
 def uvh5_to_ms(candname, candtime, dispersion_measure=None, uvh5files=None, msname=None,
-               ntbins=128, centre_time_s=CENTRE_TIME_S, template_path=TEMPLATE, singlems=False):
+               ntbins=8, centre_time_s=CENTRE_TIME_S, template_path=TEMPLATE, singlems=False):
     """Convert uvh5 to ms.
 
     This mostly follows the method used in the real-time system with some differences:
@@ -117,14 +117,14 @@ def dedisperse_and_select_times_UV(UV, dispersion_measure, centre_time, ntbins):
 
     tobs_jd = UV.time_array.reshape(UV.Ntimes, UV.Nbls)[:, 0]
     tobs = Time(tobs_jd, format='jd')
-    centre_bin = np.searchsorted(tobs.mjd, centre_time.mjd)
+    centre_tbin = np.searchsorted(tobs.mjd, centre_time.mjd)
     sample_time_ms = np.mean(np.diff(tobs.mjd))*DAY_TO_MS
 
     vis = UV.data_array.reshape(UV.Ntimes, UV.Nbls, nfreqs, UV.Npols)
     
     vis = dedisperse_and_select_times(vis, freq_GHz, sample_time_ms, dispersion_measure, centre_tbin, ntbins, REF_FREQ_GHZ)
 
-    UV.select(times=tobs_jd[centre_bin-ntbins//2:centre_bin+ntbins//2])
+    UV.select(times=tobs_jd[centre_tbin-ntbins//2:centre_tbin+ntbins//2])
 
     UV.data_array = vis.reshape(UV.Nblts, UV.Nspws, UV.Nfreqs, UV.Npols)    
 
