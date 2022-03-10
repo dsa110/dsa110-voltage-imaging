ee371e57ecf23d0e1b75e5f28c9f4db2467d39cf
Dana Simard <dana.simard@astro.caltech.edu>
2022-03-10 14:23:58 -0800

    Use T3 phasing scheme

--- scripts/voltages_to_ms_rtreftime.py
+++ scripts/voltages_to_ms_rtreftime.py
@@ -4,8 +4,8 @@ Convert voltage files to measurement sets.
 import os
 from multiprocessing import Process, Manager
 import argparse
-# from dsaT3.uvh5_to_ms import uvh5_to_ms
-from dsacalib.uvh5_to_ms import uvh5_to_ms
+from dsaT3.uvh5_to_ms import uvh5_to_ms
+#from dsacalib.uvh5_to_ms import uvh5_to_ms
 from dsaT3.voltages_to_ms import *
 import dsautils.cnf as dsc
 from astropy.time import Time
@@ -105,10 +105,10 @@ def voltages_to_ms(candname: str, datestring: str, ntint: int, start_offset: int
 
     # # Convert uvh5 files to a measurement set
     # msname = f'{system_setup.msdir}{candname}'
-    # uvh5_to_ms(cand.name, cand.time, cand.dm, uvh5params.files, msname, corrparams.reftime,
-    #            system_setup.reffreq_GHz)
+    uvh5_to_ms(cand.name, cand.time, cand.dm, uvh5params.files, msname, corrparams.reftime,
+               system_setup.reffreq_GHz)
     msname = f'{system_setup.msdir}{candname}_RT'
-    uvh5_to_ms(uvh5params.files, msname, ra=RA , dec=DEC ,refmjd=corrparams.reftime.mjd)
+    # uvh5_to_ms(uvh5params.files, msname, ra=RA , dec=DEC ,refmjd=corrparams.reftime.mjd)
 
     # # Remove hdf5 files from disk
     # for hdf5file in uvh5params.files:
