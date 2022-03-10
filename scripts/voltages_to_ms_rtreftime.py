7a9633ea992704ede03118891171d0a8c9e9ec38
Dana Simard <dana.simard@astro.caltech.edu>
2022-03-10 15:47:24 -0800

    Test

--- scripts/voltages_to_ms_rtreftime.py
+++ scripts/voltages_to_ms_rtreftime.py
@@ -107,9 +107,9 @@ def voltages_to_ms(candname: str, datestring: str, ntint: int, start_offset: int
         proc.join()
 
     # # Convert uvh5 files to a measurement set
-    msname = f'{system_setup.msdir}{candname}_RT2'
+    msname = f'{system_setup.msdir}{candname}_singlems'
     uvh5_to_ms(cand.name, cand.time, cand.dm, uvh5params.files, msname, corrparams.reftime,
-               system_setup.reffreq_GHz)
+               system_setup.reffreq_GHz, singlems=True)
     # msname = f'{system_setup.msdir}{candname}_RT'
     # uvh5_to_ms(uvh5params.files, msname, ra=RA , dec=DEC ,refmjd=corrparams.reftime.mjd)
 
