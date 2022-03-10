6712537a0ff7e1bfd9c5a43c63cdda1d9fa13791
Dana Simard <dana.simard@astro.caltech.edu>
2022-03-10 10:17:59 -0800

    Bugfix: Convert ref time to mjd for ms writing

--- scripts/voltages_to_ms_rtreftime.py
+++ scripts/voltages_to_ms_rtreftime.py
@@ -108,7 +108,7 @@ def voltages_to_ms(candname: str, datestring: str, ntint: int, start_offset: int
     # uvh5_to_ms(cand.name, cand.time, cand.dm, uvh5params.files, msname, corrparams.reftime,
     #            system_setup.reffreq_GHz)
     msname = f'{system_setup.msdir}{candname}_RT'
-    uvh5_to_ms(uvh5params.files, msname, ra=RA , dec=DEC ,refmjd=corrparams.reftime)
+    uvh5_to_ms(uvh5params.files, msname, ra=RA , dec=DEC ,refmjd=corrparams.reftime.mjd)
 
     # # Remove hdf5 files from disk
     # for hdf5file in uvh5params.files:
