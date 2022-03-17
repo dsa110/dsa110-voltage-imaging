ea4f3a56d31316f79ea025a2c6cfb61230616f46
Dana Simard <dana.simard@astro.caltech.edu>
2022-03-17 11:34:17 -0700

    First pass of pipeline with dask

---
+++ dsaT3/voltages_to_ms_dask.py
@@ -0,0 +1,326 @@
+"""Utilties for converting T3 voltage dumps to measurement sets."""
+
+from collections import namedtuple
+import time
+import re
+import os
+import asyncio
+from contextlib import contextmanager
+import subprocess
+from multiprocessing import Process
+import queue
+from pkg_resources import resource_filename
+import numpy as np
+import astropy.units as u
+from dask.distributed import Variable, Lock
+from antpos.utils import get_itrf
+from dsautils.coordinates import get_declination, get_elevation
+import dsacalib.constants as ct
+from dsaT3.generate_uvh5 import calculate_uvw_and_geodelay, get_total_delay
+from dsaT3.utils import rsync_file, load_params, get_tstart_from_json, get_DM_from_json
+from dsaT3.generate_uvh5 import generate_uvh5
+
+PARAMFILE = resource_filename('dsaT3', 'data/T3_parameters.yaml')
+
+class ProtectedVariable():
+    def __init__(self, name, initial_value=None):
+        self.name = name
+        self.variable = Variable(name)
+        self.lock = Lock(name)
+        self.variable.set(initial_value)
+
+    @contextmanager
+    def access():
+        try:
+            assert self.lock.acquire()
+            yield self.variable
+        finally:
+            self.lock.release()
+
+    def get():
+        return self.variable.get()
+
+def generate_rsync_component(local: bool) -> "Callable":
+    """Generate an rsync function."""
+
+    def rsync_all_files(item):
+        """Rsync or copy a file."""
+        srcfile, vfile = item
+        if not os.path.exists(vfile):
+            if not local:
+                rsync_file(srcfile, vfile)
+            else:
+                os.symlink(srcfile, vfile)
+        return vfile
+    return rsync_all_files
+
+def generate_correlate_component(
+        dispersion_measure: float, ntint: int, corr_ch0: dict,
+        npol: int) -> "Callable":
+    """Generate a correlator function."""
+
+    def correlate(vfile, ncorrfiles):
+        """Correlate a file."""
+        if ncorrfiles.get() > 2:
+            await asyncio.sleep(60)
+
+        corr = re.findall('corr\d\d', vfile)[0]
+        if not os.path.exists('{0}.corr'.format(vfile)):
+            first_channel_MHz = corr_ch0[corr]
+            command = (
+                '/home/ubuntu/proj/dsa110-shell/dsa110-bbproc/toolkit_dev '
+                f'-i {vfile} -o {vfile}.corr -t {ntint} -c {first_channel_MHz} '
+                f'-d delays.dat {"" if npol==4 else "-a"}')
+            if dispersion_measure is not None:
+                command += f' -m {dispersion_measure}'
+            print(command)
+            process = subprocess.Popen(
+                command,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                shell=True)
+            proc_stdout = str(process.communicate()[0].strip())
+            print(proc_stdout)
+
+        corrfile = '{0}.corr'.format(vfile)
+
+        with ncorrfiles.access() as var:
+            current_value = var.get()
+            var.set(current_value + 1)
+
+        return corrfile
+
+    return correlate
+
+def generate_uvh5_component(
+        candname: str, corrdir: str, declination: "Manager().Value", vis_params: dict,
+        start_offset: int, end_offset: int) -> "Callable":
+    """Generate a uvh5 writer."""
+
+    def write_uvh5(corrfile, ncorrfiles):
+        """Write correlated data to a uvh5 file."""
+        _uvh5name = generate_uvh5(
+            f'{corrdir}/{candname}',
+            declination.value*u.deg,
+            corrfile=corrfile,
+            vis_params=vis_params,
+            start_offset=start_offset,
+            end_offset=end_offset
+        )
+
+        os.remove(corrfile)
+        with ncorrfiles.access() as var:
+            current_value = var.get()
+            var.set(current_value - 1)
+
+    return write_uvh5
+
+def get_declination_etcd(tstart):
+    """Look up the declination from etcd."""
+    declination = get_declination(get_elevation(tstart)).to_value(u.deg)
+    return declination
+
+def generate_delay_table(vis_params, reftime, declination):
+    """Generate a table of geometric and cable delays for the correlator."""
+    _buvw, ant_bw = calculate_uvw_and_geodelay(vis_params, reftime, declination*u.deg)
+    total_delay = get_total_delay(
+        vis_params['baseline_cable_delays'], ant_bw, vis_params['bname'],
+        vis_params['antenna_order'])
+    total_delay_string = '\n'.join(total_delay.flatten().astype('str'))+'\n'
+    with open("delays.dat", "w", encoding='utf-8') as f:
+        f.write(total_delay_string)
+
+def initialize_system():
+    """Set system parameters needed in voltage to ms converter."""
+    params = load_params(PARAMFILE)
+    SystemSetup = namedtuple(
+        "SystemSetup", "T3dir archivedir corrdir msdir start_time_offset reffreq_GHz corr_ch0_MHz")
+    start_time_offset = params['burst_start_s']*u.s
+    corr_ch0_MHz = {corr: 1e3*params['f0_GHz']+params['deltaf_MHz']*corr_ch0
+                    for corr, corr_ch0 in params['ch0'].items()}
+
+    system_setup = SystemSetup(
+        params['T3dir'], params['archivedir'], params['corrdir'], params['msdir'],
+        start_time_offset, params['reffreq_GHz'], corr_ch0_MHz)
+    return system_setup
+
+def initialize_candidate(candname, datestring, system_setup, dispersion_measure=None):
+    """Set candidate parameters using information in the json header."""
+    corrlist = list(system_setup.corr_ch0_MHz.keys())
+
+    if datestring == 'current':
+        local = False
+        headerfile = f'{system_setup.t3dir}/{candname}.json'
+        voltagefiles = [f'{corr}.sas.pvt:/home/ubuntu/data/{candname}_data.out'
+                        for corr in corrlist]
+
+    else:
+        local = True
+        headerfile = f'{system_setup.archivedir}/{datestring}/{candname}.json'
+        voltagefiles = [f'{system_setup.archivedir}/{datestring}/{corr}_{candname}_data.out'
+                        for corr in corrlist]
+
+    tstart = get_tstart_from_json(headerfile)
+
+    if dispersion_measure is None:
+        dispersion_measure = get_DM_from_json(headerfile)
+        if dispersion_measure < 1:
+            dispersion_measure = None
+
+    Candidate = namedtuple('Candidate', 'name time dm voltagefiles headerfile local')
+    cand = Candidate(candname, tstart, dispersion_measure, voltagefiles, headerfile, local)
+    return cand
+
+def initialize_correlator(fullpol, ntint, cand, system_setup):
+    """Set correlator parameters."""
+    corrlist = list(system_setup.corr_ch0_MHz.keys())
+    reftime = cand.time + system_setup.start_time_offset
+    npol = 4 if fullpol else 2
+    nfint = 1 if fullpol else 8
+    corrfiles = [f'{system_setup.corrdir}/{corr}_{cand.name}_data.out' for corr in corrlist]
+
+    CorrelatorParameters = namedtuple('Correlator', 'reftime npol nfint ntint files')
+    correlator_params = CorrelatorParameters(reftime, npol, nfint, ntint, corrfiles)
+    return correlator_params
+
+def initialize_uvh5(corrparams, cand, system_setup):
+    """Set parameters for writing uvh5 files."""
+    corrlist = list(system_setup.corr_ch0_MHz.keys())
+    uvh5files = [f'{system_setup.corrdir}/{cand.name}_{corr}.hdf5' for corr in corrlist]
+    visparams = initialize_vis_params(corrparams, cand)
+
+    UVH5Parameters = namedtuple('UVH5', 'files visparams')
+    uvh5_params = UVH5Parameters(uvh5files, visparams)
+    return uvh5_params
+
+def initialize_vis_params(corrparams, cand):
+    """Set parameters that describe the visbilities produced by the correlator."""
+    T3params = load_params(PARAMFILE)
+    vis_params = parse_visibility_parameters(T3params, cand.time, corrparams.ntint)
+    vis_params['tref'] = corrparams.reftime
+    vis_params['npol'] = corrparams.npol
+    vis_params['nfint'] = corrparams.nfint
+    vis_params['ntint'] = corrparams.ntint
+    return vis_params
+
+def parse_visibility_parameters(params: dict, tstart: 'astropy.time.Time', ntint: int) -> dict:
+    """Parse details about the data to be extracted.
+
+    Parameters
+    ----------
+    params : dict
+        Parameters passed by the user definining the correlator and T3 system.
+    tstart : astropy.time.Time
+        Start time of the correlated visibility file.
+    ntint : int
+        The number of time bins integrated together in the correlated data
+        compared to the native resolution.
+
+    Returns
+    -------
+    dict
+        Parameters that describe the visibilities to be read in.
+    """
+    antenna_order = params['antennas']
+    fobs = params['f0_GHz']+params['deltaf_MHz']*1e-3*np.arange(params['nchan'])
+    nant = len(antenna_order)
+    nbls = (nant*(nant+1))//2
+
+    # Visibilities have already been integrated in time, so account for that when
+    # determining the observation times.
+    tsamp = params['deltat_s']*ntint*u.s
+    tobs = tstart + (np.arange(params['nsubint']//ntint)+0.5)*tsamp
+
+    # Get baselines
+    blen, bname = get_blen(antenna_order)
+    # Get indices for baselines to the reference antenna
+    refidxs = []
+    refant = str(antenna_order[0])
+    for i, bn in enumerate(bname):
+        if refant in bn.split('-'):
+            refidxs += [i]
+
+    cable_delays = get_cable_delays(params['outrigger_delays'], bname)
+
+    vis_params = {
+        # Baselines
+        'antenna_order': antenna_order,
+        'blen': blen,
+        'bname': bname,
+        'nbls': nbls,
+        'refidxs': refidxs,
+        'antenna_cable_delays': params['outrigger_delays'],
+        'baseline_cable_delays': cable_delays,
+        # Time
+        'tsamp': tsamp,
+        'tobs': tobs,
+        # Frequency
+        'fobs': fobs,
+        'corr_ch0': params['ch0'],
+        'nchan_corr': params['nchan_corr']}
+    return vis_params
+
+def get_cable_delays(outrigger_delays: dict, bname: list) -> np.ndarray:
+    """Calculate cable delays from the measured outrigger cable delays.
+
+    Antenna names in both input parameters are indexed at 1.
+
+    Parameters
+    ----------
+    outrigger_delays : dict
+        The delay for each antenna.  Missing keys have 0 delay.
+    bname : list
+        The name of each baseline.
+
+    Returns
+    -------
+    np.ndarray
+        The cable delay for each baseline in bname.
+    """
+    delays = np.zeros(len(bname), dtype=np.int)
+    for i, bn in enumerate(bname):
+        ant1, ant2 = bn.split('-')
+        delays[i] = outrigger_delays.get(int(ant1), 0)-\
+                    outrigger_delays.get(int(ant2), 0)
+    return delays
+
+def get_blen(antennas: list) -> tuple:
+    """Gets the baseline lengths for a subset of antennas.
+
+    Parameters
+    ----------
+    antennas : list
+        The antennas used in the array.
+
+    Returns
+    -------
+    blen : array
+        The ITRF coordinates of all of the baselines.
+    bname : list
+        The names of all of the baselines.
+    """
+    ant_itrf = get_itrf(
+        latlon_center=(ct.OVRO_LAT*u.rad, ct.OVRO_LON*u.rad, ct.OVRO_ALT*u.m)
+    ).loc[antennas]
+    xx = np.array(ant_itrf['dx_m'])
+    yy = np.array(ant_itrf['dy_m'])
+    zz = np.array(ant_itrf['dz_m'])
+    # Get uvw coordinates
+    nants = len(antennas)
+    nbls = (nants*(nants+1))//2
+    blen = np.zeros((nbls, 3))
+    bname = []
+    k = 0
+    for i in range(nants):
+        for j in range(i, nants):
+            blen[k, :] = np.array([
+                xx[i]-xx[j],
+                yy[i]-yy[j],
+                zz[i]-zz[j]
+            ])
+            bname += ['{0}-{1}'.format(
+                antennas[i],
+                antennas[j]
+            )]
+            k += 1
+    return blen, bname
---
+++ scripts/voltages_to_ms_dask.py
@@ -0,0 +1,168 @@
+"""
+Convert voltage files to measurement sets.
+"""
+import os
+from multiprocessing import Process, Manager
+import argparse
+from dsaT3.uvh5_to_ms import uvh5_to_ms
+from dsaT3.voltages_to_ms import *
+
+def voltages_to_ms(candname: str, datestring: str, ntint: int, start_offset: int, end_offset: int,
+                   dispersion_measure: float=None, full_pol: bool=False, continuum_source: bool=False) -> None:
+    """
+    Correlate voltage files and convert to a measurement set.
+
+    Parameters
+    ----------
+    candname : str
+        The unique name of the candidate.
+    datestring : str
+        The datestring the observation is archived under. Use 'current' if the
+        data is from the current, unarchived observing run.
+    ntint : int
+        The number of time samples to integrate together during correlation.
+    nfint : int
+        The number of frequency channels to integrate together after removing
+        outrigger delays.
+    start_offset : int
+        The number of time samples (after correlation) to offset the start of
+        the measurement set by.  If not provided, the entire time is converted
+        to a measurement set.
+    end_offset : int
+        The last time sample (after correlation) to write to the measurement
+        set. If not provide,d the entire time is converted to a measurement
+        set.
+    dispersion_measure: float
+        Overrides the dispersion measure in the json header file.
+    full_pol: bool
+        If True, no frequency averaging is done, and 4 polarizations are written out.
+        If False, frequency averaging x 8 is done, and XX and YY are written out.
+    """
+
+    start_offset, end_offset = set_default_if_unset(start_offset, end_offset)
+    system_setup = initialize_system()
+    cand = initialize_candidate(candname, datestring, system_setup, dispersion_measure)
+    corrparams = initialize_correlator(full_pol, ntint, cand, system_setup)
+    uvh5params = initialize_uvh5(corrparams, cand, system_setup)
+
+    # Initialize the process manager, locks, values, and queues
+    manager = Manager()
+    ncorrfiles = manager.Value('i', 0)
+    ncorrfiles_lock = manager.Lock()
+    declination = manager.Value(float, None)
+    declination_lock = manager.Lock()
+    rsync_queue = manager.Queue()
+    corr_queue = manager.Queue()
+    uvh5_queue = manager.Queue()
+
+    # Generate the table needed by the correlator
+    get_declination_etcd = process_join(generate_declination_component(
+        declination, declination_lock, cand.time))
+    _ = get_declination_etcd()
+
+    generate_delay_table(uvh5params.visparams, corrparams.reftime, declination.value)
+
+    rsync_all_files = pipeline_component(
+        generate_rsync_component(cand.local),
+        rsync_queue,
+        corr_queue)
+
+    correlate = pipeline_component(
+        generate_correlate_component(
+            cand.dm, corrparams.ntint, system_setup.corr_ch0_MHz,
+            corrparams.npol, ncorrfiles, ncorrfiles_lock),
+        corr_queue,
+        uvh5_queue)
+
+    write_uvh5 = pipeline_component(
+        generate_uvh5_component(
+            cand.name, system_setup.corrdir, declination, uvh5params.visparams,
+            start_offset, end_offset, ncorrfiles, ncorrfiles_lock),
+        uvh5_queue)
+
+    processes = [
+        Process(target=rsync_all_files, daemon=True),
+        Process(target=correlate, daemon=True),
+        Process(target=write_uvh5, daemon=True)]
+
+    # Populate rsync queue (the entry queue in our pipeline)
+    # We only need to do this if the corresponding hdf5 file hasn't
+    # been created already.
+    for i, filename in enumerate(cand.voltagefiles):
+        if not os.path.exists(uvh5params.files[i]):
+            rsync_queue.put([filename, corrparams.files[i]])
+    rsync_queue.put('END')
+
+    # Start all processes in the pipeline
+    for proc in processes:
+        proc.start()
+
+    # Wait for all processes to finish
+    for proc in processes:
+        proc.join()
+
+    # Convert uvh5 files to a measurement set
+    msname = f'{system_setup.msdir}{candname}'
+    ntbins = None if continuum_source else 8
+    uvh5_to_ms(cand.name, cand.time, uvh5params.files, msname, corrparams.reftime, ntbins)
+
+    # Remove hdf5 files from disk
+    for hdf5file in uvh5params.files:
+        os.remove(hdf5file)
+
+def set_default_if_unset(start_offset: int, end_offset: int) -> tuple:
+    """Set default `start_offset` and `end_offset` if they aren't set."""
+    if start_offset < 0:
+        start_offset = None
+    if end_offset < 0:
+        end_offset = None
+    return start_offset, end_offset
+
+def parse_commandline_arguments() -> "argparse.Namespace":
+    """Parse commandline arguments."""
+    parser = argparse.ArgumentParser(
+        description='Correlate candidate voltage files.')
+    parser.add_argument(
+        'candname',
+        type=str,
+        help='unique candidate name')
+    parser.add_argument(
+        '--datestring',
+        type=str,
+        help='datestring of archived candidate',
+        nargs='?',
+        default='current')
+    parser.add_argument(
+        '--ntint',
+        type=int,
+        nargs='?',
+        default=8,
+        help='number of native time bins to integrate during correlation')
+    parser.add_argument(
+        '--startoffset',
+        type=int,
+        nargs='?',
+        default=1716,
+        help='nbins from beginning of correlated data to start writing to ms')
+    parser.add_argument(
+        '--stopoffset',
+        type=int,
+        nargs='?',
+        default=4788,
+        help='number of bins from end of correlation to write to ms')
+    parser.add_argument(
+        '--dm',
+        type=float,
+        nargs='?',
+        help='dispersion measure to use instead of value in header file')
+    parser.add_argument('--continuum', action='store_true')
+    parser.set_defaults(continuum=False)
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    ARGS = parse_commandline_arguments()
+    voltages_to_ms(ARGS.candname, ARGS.datestring, ntint=ARGS.ntint,
+                   start_offset=ARGS.startoffset, end_offset=ARGS.stopoffset,
+                   dispersion_measure=ARGS.dm, continuum_source=ARGS.continuum)
