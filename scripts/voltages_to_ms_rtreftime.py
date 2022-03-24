08867f8ef964dacac660e10c61bc60b653527b55
ubuntu <ubuntu@pm.me>
2022-03-24 16:35:39 +0000

    Correlating without delays

--- scripts/voltages_to_ms_rtreftime.py
+++ scripts/voltages_to_ms_rtreftime.py
@@ -15,6 +15,8 @@ import astropy.units as u
 RA, DEC =  85.650625*u.deg, 49.85213889*u.deg
 # 3C295
 # RA, DEC = 212.8359583333333*u.deg, 52.2025*u.deg
+# 3C196
+# RA, DEC = 123.40029167*u.deg, 48.21719444*u.deg
 
 def voltages_to_ms(candname: str, datestring: str, ntint: int, start_offset: int, end_offset: int,
                    full_pol: bool=False) -> None:
@@ -108,7 +110,7 @@ def voltages_to_ms(candname: str, datestring: str, ntint: int, start_offset: int
     # msname = f'{system_setup.msdir}{candname}_nointerp'
     # uvh5_to_ms(cand.name, cand.time, cand.dm, uvh5params.files, msname, corrparams.reftime,
     #            system_setup.reffreq_GHz)
-    msname = f'{system_setup.msdir}{candname}_RT_notdev'
+    msname = f'{system_setup.msdir}{candname}_RT_nodelays'
     uvh5_to_ms(uvh5params.files, msname, ra=RA , dec=DEC ,refmjd=corrparams.reftime.mjd)
 
     # # Remove hdf5 files from disk
