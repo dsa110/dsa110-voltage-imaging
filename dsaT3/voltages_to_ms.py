acf317fe147212c95af307319825d0d0f213ea42
Dana Simard <dana.simard@astro.caltech.edu>
2022-03-24 12:38:36 -0700

    Add back in delay removal

--- dsaT3/voltages_to_ms.py
+++ dsaT3/voltages_to_ms.py
@@ -167,8 +167,6 @@ def generate_delay_table(vis_params, reftime, declination):
     total_delay = get_total_delay(
         vis_params['baseline_cable_delays'], ant_bw, vis_params['bname'],
         vis_params['antenna_order'])
-    # TODO: Remove the dummy total delay
-    total_delay = np.zeros(total_delay.shape, total_delay.dtype)
     total_delay_string = '\n'.join(total_delay.flatten().astype('str'))+'\n'
     with open("delays.dat", "w", encoding='utf-8') as f:
         f.write(total_delay_string)
