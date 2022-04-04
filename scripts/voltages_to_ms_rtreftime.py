b0b92926149bbc23b0ca1d4bee40a0fe49b66053
Dana Simard <dana.simard@astro.caltech.edu>
2022-04-04 14:43:52 -0700

    Bugfix: Default value for full_pol

--- scripts/voltages_to_ms_rtreftime.py
+++ scripts/voltages_to_ms_rtreftime.py
@@ -20,7 +20,7 @@ import astropy.units as u
 # RA, DEC = 123.40029167*u.deg, 48.21719444*u.deg
 
 def voltages_to_ms(candname: str, datestring: str, ntint: int, start_offset: int, end_offset: int,
-                   dispersion_measure: float=None) -> None:
+                   dispersion_measure: float=None, full_pol=False) -> None:
     """
     Correlate voltage files and convert to a measurement set.
 
